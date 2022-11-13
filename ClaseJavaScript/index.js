f2f45b7c0638bed770ced63f8c685dbd37bd60e9
Bora980 <lx15712@innovaccion.mx>
2022-11-13 16:14:49 +0100

    Practicas LaunchX Mission Commander "Jesús Guzmán"

--- ClaseJavaScript/index.js
+++ ClaseJavaScript/index.js
@@ -1,1 +1,18 @@
-alert("Hola mundo");
+parrafoInicial = docement.gerElementById("parrafoinicial");
+
+/*
+CASES
+
+PascalCase - Clases - JAVA
+
+camelCase - variables, funciones, atributos. - JavaScript
+    variable -> parrafoInicial =
+    funvion -> parrafoInicial()
+    atibuto -> parrafoInicial
+
+snake_cae - Python, JavaScript
+    variables y funciones
+
+CASE - Constantes
+
+*/
